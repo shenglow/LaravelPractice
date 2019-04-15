e518225081ee861074524ec33753c305476d3efe
shenglow <shenglow1993@gmail.com>
2019-04-15 14:59:11 +0800

    add table schema

---
+++ database/migrations/2019_04_15_063946_create_home_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHomeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->text('content');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExits('home');
+    }
+}
